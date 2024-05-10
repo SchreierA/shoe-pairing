622e02a556ca19b66f9e8e29a01fb74e3951443a
Attila Schreier <schreiera13@gmail.com>
2024-05-10 18:50:14 +0200

    implements a safety check on the game page

--- src/app/components/game-page/game-page.component.ts
+++ src/app/components/game-page/game-page.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-page',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './game-page.component.html',
   styleUrl: './game-page.component.scss',
 })
-export class GamePageComponent {}
+export class GamePageComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      this.handleFallback(params);
+    });
+  }
+
+  handleFallback(params: Params) {
+    const size = params['size'];
+    const sizeTooBig = size > 20;
+    const sizeTooSmall = size < 6;
+    const sizeIsOdd = size % 2;
+    if (!size || sizeIsOdd || sizeTooBig || sizeTooSmall) {
+      this.router.navigate(['']);
+    }
+  }
+}
